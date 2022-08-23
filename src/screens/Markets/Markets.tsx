37d447221cce1bf9bf30d2af75d6a6bf08db5fb0
amorfatih <fatihermetin@gmail.com>
2022-08-24 00:34:39 +0300

    Add cryptocurrency latest swr hook to Markets screen

--- src/screens/Markets/Markets.tsx
+++ src/screens/Markets/Markets.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useLatestCCList } from "../../swr/useLatestCCList";
 import BaseScreenContainer from "../BaseScreenContainer";
 
 const Markets = () => {
+  const { data, error, isValidating } = useLatestCCList();
+
   return <BaseScreenContainer></BaseScreenContainer>;
 };
 
