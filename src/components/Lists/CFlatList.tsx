bfc81840d4d62c5d3ad9fd2f17340f9d697df9e7
amorfatih <fatihermetin@gmail.com>
2022-08-24 23:55:44 +0300

    Add base CFlatList

---
+++ src/components/Lists/CFlatList.tsx
@@ -0,0 +1,8 @@
+import { FlatList as NBFlatList } from "native-base";
+import React from "react";
+
+const CFlatList = (props) => {
+  return <NBFlatList {...props} />;
+};
+
+export default CFlatList;
