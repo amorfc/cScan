5e6bb0e56021c7ba6054f5f125df57a17b8043f5
amorfatih <fatihermetin@gmail.com>
2022-08-25 23:20:36 +0300

    Fix statusbar bg color for ios

--- src/components/StatusBar/CStatusBar.tsx
+++ src/components/StatusBar/CStatusBar.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { useColorModeValue } from "native-base";
+import { useColorModeValue, useSafeArea } from "native-base";
+import CBox from "../Box/CBox";
 
 const CStatusBar = () => {
   //Provides the text color to the status bar
   const barStyle = useColorModeValue("dark", "light");
-
-  return <StatusBar style={barStyle} />;
+  const safeAreaProps = useSafeArea({
+    safeAreaTop: true,
+  });
+  return (
+    <CBox {...safeAreaProps}>
+      <StatusBar style={barStyle} />
+    </CBox>
+  );
 };
 
 export default CStatusBar;
