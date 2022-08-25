616061a73c11b31e719bf0dad1127655539a6c87
amorfatih <fatihermetin@gmail.com>
2022-08-25 22:50:51 +0300

    Add app container component with CStatusbar component

---
+++ src/AppContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CStatusBar from "./components/StatusBar/CStatusBar";
+import Navigation from "./navigation/Navigation";
+
+const AppContainer = () => {
+  return (
+    <>
+      <CStatusBar />
+      <Navigation />
+    </>
+  );
+};
+
+export default AppContainer;
