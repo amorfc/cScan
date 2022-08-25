f9262813eb6484fd2e195decb46b4a7bcb75474f
amorfatih <fatihermetin@gmail.com>
2022-08-26 01:08:48 +0300

    Add status bar animation on hide & android bgColor

--- src/components/StatusBar/CStatusBar.tsx
+++ src/components/StatusBar/CStatusBar.tsx
@@ -1,17 +1,28 @@
-import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { useColorModeValue, useSafeArea } from "native-base";
+import React from "react";
+import { useCRouteCUtils } from "../../hooks/useCRouteCUtils";
 import CBox from "../Box/CBox";
 
+//TODO - Move all hard coded strings to theme&color definitions
 const CStatusBar = () => {
   //Provides the text color to the status bar
   const barStyle = useColorModeValue("dark", "light");
   const safeAreaProps = useSafeArea({
     safeAreaTop: true,
   });
+  const { statusBarHidden: isHidden } = useCRouteCUtils();
+  const backgroundColor = isHidden ? "black" : "white";
+
   return (
-    <CBox {...safeAreaProps}>
-      <StatusBar style={barStyle} />
+    <CBox bg={backgroundColor} {...safeAreaProps}>
+      <StatusBar
+        animated
+        backgroundColor={backgroundColor}
+        hideTransitionAnimation={"slide"}
+        style={isHidden ? "default" : barStyle}
+        hidden={isHidden}
+      />
     </CBox>
   );
 };
