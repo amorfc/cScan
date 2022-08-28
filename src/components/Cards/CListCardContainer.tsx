2f74488c3328458835779e3b5328cbdb059df2d8
amorfatih <fatihermetin@gmail.com>
2022-08-28 18:42:44 +0300

    Add list card container component

---
+++ src/components/Cards/CListCardContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import BaseContainer from "../Container/BaseContainer";
+
+const CListCardContainer = (props) => {
+  return <BaseContainer>{props.children}</BaseContainer>;
+};
+
+export default CListCardContainer;
