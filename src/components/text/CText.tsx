c2166db77b6d14042ad605de834b19cc4afb25c6
amorfatih <fatihermetin@gmail.com>
2022-09-06 21:58:38 +0300

    Add text size font size enums

--- src/components/text/CText.tsx
+++ src/components/text/CText.tsx
@@ -1,13 +1,32 @@
 import { Text } from "native-base";
 import React from "react";
+import { TextProps } from "react-native";
 
-interface CTextProps {
+export enum CTextFontSize {
+  "SM" = "sm",
+  "2XS" = "2xs",
+  "XS" = "xs",
+  "MD" = "md",
+  "XL" = "xl",
+  "LG" = "lg",
+  "XL2" = "2xl",
+  "3XL" = "3xl",
+  "4XL" = "4xl",
+  "5XL" = "5xl",
+  "6XL" = "6xl",
+  "7XL" = "7xl",
+  "8XL" = "8xl",
+  "9XL" = "9xl",
+}
+
+interface CTextProps extends TextProps {
   text: string;
+  fontSize?: CTextFontSize;
 }
 
 const CText = (props: CTextProps) => {
   const { text } = props;
-  return <Text>{text}</Text>;
+  return <Text {...props}>{text}</Text>;
 };
 
 export default CText;
