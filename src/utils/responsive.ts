15674620d1f5cf17732c7583045f9e7cb5757ee4
amorfatih <fatihermetin@gmail.com>
2022-09-06 21:58:21 +0300

    Change RFontSPercentage util with device height&white

--- src/utils/responsive.ts
+++ src/utils/responsive.ts
@@ -1,13 +1,4 @@
-import { Dimensions, Platform, StatusBar } from "react-native";
-import { isIOS } from "./common";
+import { Dimensions } from "react-native";
 
-export const RFontSPercentage = (size: number) => {
-  const { height, width } = Dimensions.get("window");
-  const standardLength = width > height ? width : height;
-  const offset = (width > height ? 0 : Platform.OS === "ios" ? 78 : StatusBar.currentHeight) ?? 0; // iPhone X style SafeAreaView size in portrait
-
-  const deviceHeight = !isIOS() ? standardLength - offset : standardLength;
-
-  const heightPercent = (size * deviceHeight) / 100;
-  return Math.round(heightPercent);
-};
+export const deviceHeight = Dimensions.get("window").height;
+export const deviceWidth = Dimensions.get("window").width;
