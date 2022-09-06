9ece3640b15cb2b8b7a2a7160731a5886c18998a
amorfatih <fatihermetin@gmail.com>
2022-09-06 22:13:44 +0300

    CText font size enum

--- src/components/text/CText.tsx
+++ src/components/text/CText.tsx
@@ -3,20 +3,20 @@ import React from "react";
 import { TextProps } from "react-native";
 
 export enum CTextFontSize {
-  "SM" = "sm",
-  "2XS" = "2xs",
-  "XS" = "xs",
-  "MD" = "md",
-  "XL" = "xl",
-  "LG" = "lg",
-  "XL2" = "2xl",
-  "3XL" = "3xl",
-  "4XL" = "4xl",
-  "5XL" = "5xl",
-  "6XL" = "6xl",
-  "7XL" = "7xl",
-  "8XL" = "8xl",
-  "9XL" = "9xl",
+  SM = "sm",
+  XS2 = "2xs",
+  XS = "xs",
+  MD = "md",
+  XL = "xl",
+  LG = "lg",
+  XL2 = "2xl",
+  XL3 = "3xl",
+  XL4 = "4xl",
+  XL5 = "5xl",
+  XL6 = "6xl",
+  XL7 = "7xl",
+  XL8 = "8xl",
+  XL9 = "9xl",
 }
 
 interface CTextProps extends TextProps {
