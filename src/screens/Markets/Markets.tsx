8683bfd54cc1262b511eb843bab517e6c1734002
amorfatih <fatihermetin@gmail.com>
2022-09-07 19:49:34 +0300

    Add exchange list to main tab view

--- src/screens/Markets/Markets.tsx
+++ src/screens/Markets/Markets.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import CTabView from "../../components/TabView/CTabView";
 import CScreenContainer from "../CScreenContainer";
+import ExchangeList from "../Exchanges/ExchangeList";
 import MarketList from "./MarketList";
 
 const TAB_VIEW_ROUTES_KEY = {
   MARKET_LIST: "market_list",
+  EXCHANGE_LIST: "exchanges_list",
 };
 
 const Markets = () => {
   const renderTabs = [
     {
-      title: "Market",
+      title: "Markets",
       render: <MarketList />,
       key: TAB_VIEW_ROUTES_KEY.MARKET_LIST,
     },
     {
-      title: "Market2",
-      render: <MarketList />,
-      key: "temp1",
+      title: "Exchanges",
+      render: <ExchangeList />,
+      key: TAB_VIEW_ROUTES_KEY.EXCHANGE_LIST,
     },
   ];
 
