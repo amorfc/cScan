7e2ec28638b741340631da5bb21c5802313ba9c9
amorfatih <fatihermetin@gmail.com>
2022-09-13 23:23:35 +0300

    Wrap AssetCard with CPressable to onPress action

--- src/components/Cards/AssetCard.tsx
+++ src/components/Cards/AssetCard.tsx
@@ -1,7 +1,9 @@
 import { Center, Flex, Stack } from "native-base";
 import React, { useMemo } from "react";
 import { Quote } from "../../models/domainModels";
+import { navigate } from "../../navigation/navUtils";
 import CImage from "../Image/CImage";
+import CPressable from "../Pressable/CPressable";
 import CText from "../Text/CText";
 import CListCardContainer from "./CListCardContainer";
 
@@ -25,28 +27,34 @@ const AssetCard = (props: AssetCardProps) => {
 
   const logoUrl = useMemo(() => getAssetLogo(id), [id]);
 
+  const onCardPress = () => {
+    navigate({ name: "AssetDetail" });
+  };
+
   return (
     <CListCardContainer>
-      <Stack direction={["row", "row", "row"]}>
-        <Flex flex={2} justifyContent={"center"} alignItems={"center"}>
-          <CText text={rank?.toString()} />
-        </Flex>
-        <Stack flex={6} direction={"row"} justifyContent={"space-between"}>
-          <Stack flex={1} justifyContent={"center"} alignItems={"center"}>
-            <CImage url={logoUrl} />
+      <CPressable onPress={onCardPress}>
+        <Stack direction={["row", "row", "row"]}>
+          <Flex flex={2} justifyContent={"center"} alignItems={"center"}>
+            <CText text={rank?.toString()} />
+          </Flex>
+          <Stack flex={6} direction={"row"} justifyContent={"space-between"}>
+            <Stack flex={1} justifyContent={"center"} alignItems={"center"}>
+              <CImage url={logoUrl} />
+            </Stack>
+            <Center flex={2} alignItems={"flex-start"}>
+              <CText text={symbol} />
+              <CText text={marketCap?.toString()} />
+            </Center>
           </Stack>
-          <Center flex={2} alignItems={"flex-start"}>
-            <CText text={symbol} />
-            <CText text={marketCap?.toString()} />
+          <Center flex={4} alignItems={"flex-end"} pr={2}>
+            <CText text={price?.toString()} />
+          </Center>
+          <Center flex={4}>
+            <CText text={percentChange?.toString()} />
           </Center>
         </Stack>
-        <Center flex={4} alignItems={"flex-end"} pr={2}>
-          <CText text={price?.toString()} />
-        </Center>
-        <Center flex={4}>
-          <CText text={percentChange?.toString()} />
-        </Center>
-      </Stack>
+      </CPressable>
     </CListCardContainer>
   );
 };
