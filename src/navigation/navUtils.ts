776ef5420b5b77980badfef7b4e4b77471999066
amorfatih <fatihermetin@gmail.com>
2022-09-13 23:16:25 +0300

    Add global navigate method

--- src/navigation/navUtils.ts
+++ src/navigation/navUtils.ts
@@ -2,6 +2,10 @@ import { createNavigationContainerRef } from "@react-navigation/native";
 
 export const navigationRef = createNavigationContainerRef();
 
+export const navigate = ({ name, params }: { name; params? }) => {
+  navigationRef.current?.navigate(name, params);
+};
+
 export const isNavigationReady = () => navigationRef?.isReady();
 
 export const defaultScreenOptions = () => ({
